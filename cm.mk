cfaea663911a81bcd9b1ebd6d0c25faf7cf11148
Sandeep Sethi <adeveloper79@gmail.com>
2016-12-25 11:22:40 +0530

    Update and rename xosp.mk to cm.mk

--- cm.mk
+++ cm.mk
@@ -14,19 +14,15 @@
 
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
-# Inherit some common XOSP stuff.
-$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
-
-# Inherit some proprietary XOSP stuff.
-$(call inherit-product, vendor/xosp/config/xosp.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
-PRODUCT_NAME := xosp_tomato
+PRODUCT_NAME := cm_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
-RELEASE_TYPE := XOSP_UNOFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-micromax
 
